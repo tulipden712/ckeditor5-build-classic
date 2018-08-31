dea5ecb167b33f60b4360636263da8dafea10677
Nguyen Phong <phong.nguyenvan@gmail.com>
2018-08-31 14:38:10 +0700

    new build for react

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -9,8 +9,15 @@ import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classicedi
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Font from '@ckeditor/ckeditor5-font/src/font';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
@@ -22,16 +29,26 @@ import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight'
+import Table from '@ckeditor/ckeditor5-table/src/table'
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
+import HeadingButtonsUI from '@ckeditor/ckeditor5-heading/src/headingbuttonsui';
+import ParagraphButtonUI from '@ckeditor/ckeditor5-paragraph/src/paragraphbuttonui';
 
-export default class ClassicEditor extends ClassicEditorBase {}
+import CKEditor from '@ckeditor/ckeditor5-react'
+
+export const CKEditorReact = CKEditor
+
+export default class ClassicEditor extends ClassicEditorBase { }
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
+	CKEditor,
 	Essentials,
 	UploadAdapter,
 	Autoformat,
-	Bold,
-	Italic,
+	Bold, Italic, Underline, Strikethrough, Code,
 	BlockQuote,
 	EasyImage,
 	Heading,
@@ -42,7 +59,13 @@ ClassicEditor.builtinPlugins = [
 	ImageUpload,
 	Link,
 	List,
-	Paragraph
+	Paragraph,
+	Table,
+	TableToolbar,
+	Highlight,
+	Font,
+	Alignment,
+	BlockToolbar, ParagraphButtonUI, HeadingButtonsUI,
 ];
 
 // Editor configuration.
@@ -51,15 +74,20 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
-			'bold',
-			'italic',
+			'bold', 'italic', 'underline', 'strikethrough', 'code',
+			'|',
+			'fontSize', 'fontFamily',
 			'link',
 			'bulletedList',
 			'numberedList',
+			'alignment',
 			'imageUpload',
+			'insertTable',
 			'blockQuote',
+			'highlight',
 			'undo',
-			'redo'
+			'redo',
+			''
 		]
 	},
 	image: {
@@ -70,6 +98,52 @@ ClassicEditor.defaultConfig = {
 			'imageTextAlternative'
 		]
 	},
+	table: {
+		toolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+	},
+	highlight: {
+		options: [
+			{
+				model: 'greenMarker',
+				class: 'marker-green',
+				title: 'Green marker',
+				color: 'var(--ck-highlight-marker-green)',
+				type: 'marker'
+			},
+			{
+				model: 'redPen',
+				class: 'pen-red',
+				title: 'Red pen',
+				color: 'var(--ck-highlight-pen-red)',
+				type: 'pen'
+			}
+		]
+	},
+	fontSize: {
+		options: [
+			9,
+			11,
+			13,
+			'default',
+			17,
+			19,
+			21
+		]
+	},
+	fontFamily: {
+		options: [
+			'default',
+			'Ubuntu, Arial, sans-serif',
+			'Ubuntu Mono, Courier New, Courier, monospace'
+		]
+	},
+	blockToolbar: [
+		'paragraph', 'heading1', 'heading2', 'heading3',
+		'|',
+		'bulletedList', 'numberedList',
+		'|',
+		'blockQuote', 'imageUpload'
+	],
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
